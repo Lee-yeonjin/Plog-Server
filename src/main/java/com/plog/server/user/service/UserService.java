c085a547d15c7d28e426600e6b2d405420e3b492
namgoong <cookie_31@naver.com>
2024-07-17 19:27:14 +0900

    userservice 어노테이션 수정

--- src/main/java/com/plog/server/user/service/UserService.java
+++ src/main/java/com/plog/server/user/service/UserService.java
@@ -1,7 +1,13 @@
 package com.plog.server.user.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
 public class UserService {
 }
