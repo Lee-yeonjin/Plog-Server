62717e2b73a410c99b9d35cf4edd19c1ef984467
namgoong <cookie_31@naver.com>
2024-09-14 17:12:20 +0900

    fix: trashRequet 타입 변경

--- src/main/java/com/plog/server/trash/api/TrashController.java
+++ src/main/java/com/plog/server/trash/api/TrashController.java
@@ -1,5 +1,7 @@
 package com.plog.server.trash.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.plog.server.global.ApiResponse;
 import com.plog.server.global.util.s3File.dto.S3FileResponse;
 import com.plog.server.global.util.s3File.service.S3UploaderService;
@@ -29,14 +31,22 @@ public class TrashController {
     @PostMapping("/{uuid}/{activityid}")
     public ResponseEntity<ApiResponse<S3FileResponse>> createTrash(@PathVariable UUID uuid, @PathVariable Long activityid,
                                                                    @RequestPart(value = "image", required = false) MultipartFile image,
-                                                                   @RequestPart(value = "trashRequest") TrashRequest trashRequest) {
+                                                                   @RequestPart(value = "trashRequest") String trashRequestJson) {
 
         Activity activity = activityRepository.findById(activityid)
                 .orElseThrow(() -> new IllegalArgumentException("Activity not found"));
 
+        TrashRequest trashRequest;
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            trashRequest = objectMapper.readValue(trashRequestJson, TrashRequest.class);
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>("json처리 실패", null));
+        }
+
         S3FileResponse s3FileResponse = trashService.createTrash(trashRequest, activity, uuid, image);
 
-        return ResponseEntity.ok(new ApiResponse<>("쓰레기 및 사진 저장 완료", s3FileResponse));
+        return ResponseEntity.ok(new ApiResponse<>("success", s3FileResponse));
 
     }
 
